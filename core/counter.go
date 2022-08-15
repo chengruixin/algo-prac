39f820d2dee97d093d73dc7f632a8543d06543a4
chengruixin <chengruixin_2010@163.com>
2022-08-15 23:54:39 +0800

    feat: add heap struct

--- core/counter.go
+++ core/counter.go
@@ -33,13 +33,14 @@ func areEquals(arr1, arr2 []int) bool {
 	return true
 }
 func Test() {
-	arr := randGenArr(2e7)
+	arr := randGenArr(3e7)
 	test0 := copySlice(arr)
 	test1 := copySlice(arr)
+	test2 := copySlice(arr)
 
 	sg := sync.WaitGroup{}
 
-	sg.Add(2)
+	sg.Add(3)
 
 	go func() {
 		s := time.Now()
@@ -55,6 +56,30 @@ func Test() {
 		sg.Done()
 	}()
 
+	go func() {
+		s := time.Now()
+		test2 = NewHeap(test2, func(a, b int) int {
+			return a - b
+		}).Sort()
+		fmt.Println("heap", time.Since(s))
+		sg.Done()
+	}()
+
 	sg.Wait()
-	fmt.Println(areEquals(test0, test1))
+	fmt.Println(areEquals(test0, test1), areEquals(test1, test2))
+}
+
+func HeapTest() {
+	hps := NewHeap([]int{45, 23, 12, 34, 21, 45}, func(a, b int) int {
+		return a - b
+	})
+
+	hps.Push(23)
+	hps.Push(85)
+	hps.Push(255)
+	hps.Push(431)
+	hps.Push(1)
+
+	fmt.Println(hps.Sort())
+
 }
---
+++ core/heap.go
@@ -0,0 +1,88 @@
+package core
+
+type HeapStruct struct {
+	arr        []int
+	comparator func(a, b int) int
+}
+
+func NewHeap(arr []int, comparator func(a, b int) int) *HeapStruct {
+	hs := &HeapStruct{
+		arr:        arr,
+		comparator: comparator,
+	}
+
+	lastEffective := (len(arr) - 2) / 2
+
+	for i := lastEffective; i >= 0; i-- {
+		hs.heapify(i)
+	}
+
+	return hs
+}
+
+func (hs *HeapStruct) Size() int {
+	return len(hs.arr)
+}
+
+func (hs *HeapStruct) Sort() []int {
+	// original := []int{}
+	// copy(original, hs.arr)
+
+	res := []int{}
+
+	for hs.Size() > 0 {
+		top := hs.Pop()
+		res = append(res, top)
+	}
+
+	// hs.arr = original
+
+	return res
+}
+
+func (hs *HeapStruct) Peek() int {
+	return hs.arr[0]
+}
+
+func (hs *HeapStruct) Pop() int {
+	res := hs.Peek()
+	swap(hs.arr, 0, hs.Size()-1)
+	hs.arr = hs.arr[:hs.Size()-1]
+	hs.heapify(0)
+	return res
+}
+
+func (hs *HeapStruct) Push(node int) {
+	hs.arr = append(hs.arr, node)
+	hs.heapInsert(hs.Size() - 1)
+}
+
+func (hs *HeapStruct) heapify(node int) {
+	for l, r := node*2+1, node*2+2; l < hs.Size(); l, r = node*2+1, node*2+2 {
+		var best int
+		if r < hs.Size() {
+			if hs.comparator(hs.arr[node], hs.arr[l]) <= 0 && hs.comparator(hs.arr[node], hs.arr[r]) <= 0 {
+				break
+			}
+			if hs.comparator(hs.arr[l], hs.arr[node]) <= 0 && hs.comparator(hs.arr[l], hs.arr[r]) <= 0 {
+				best = l
+			} else {
+				best = r
+			}
+		} else {
+			if hs.comparator(hs.arr[node], hs.arr[l]) <= 0 {
+				break
+			}
+			best = l
+		}
+		swap(hs.arr, node, best)
+		node = best
+	}
+}
+
+func (hs *HeapStruct) heapInsert(node int) {
+	for parent := (node - 1) / 2; node > 0 && parent >= 0 && hs.comparator(hs.arr[node], hs.arr[parent]) <= 0; parent = (node - 1) / 2 {
+		swap(hs.arr, node, parent)
+		node = parent
+	}
+}
